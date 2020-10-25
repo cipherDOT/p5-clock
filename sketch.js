8abe0751bb0cc28f39e4ca872710ec1c18d1c213
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2020-10-25 12:46:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,32 @@
+// This is a bassic model of clock. I just followed along with Daniel Shiffman, a wonderful programmer, a creative coder.
+// https://www.youtube.com/thecodingtrain --> channel Daniel Shiffman
+// https://www.youtube.com/watch?v=E4RyStef-gY&t=474s --> video link
+// I am just starting out on JavaScript and wanted to learn p5.js
+
 function setup() {
-    createCanvas(400, 400);
-    angleMode(DEGREES)
+    createCanvas(window.innerWidth, window.innerHeight);
+    angleMode(DEGREES);
 }
 
 function draw() {
     background(0);
-    translate(200, 200);
+    translate(width / 2, height / 2);
     rotate(-90);
 
-    let hr = hour()
-    let mn = minute()
-    let sec = second()
+    let hr = hour();
+    let mn = minute();
+    let sec = second();
 
     // strokeWeight(4);
     // stroke(255);
     // noFill();
-    // ellipse(200, 200, 300, 300);
+    // ellipse(0, 0, 300, 300);
 
     strokeWeight(8);
     noFill();
     // second hand
     stroke(255, 100, 100);
-    let secondhand = map(sec, 0, 60, 0, 360)
+    let secondhand = map(sec, 0, 60, 0, 360);
     arc(0, 0, 300, 300, 0, secondhand);
 
     push();
@@ -52,8 +57,9 @@ function draw() {
     line(0, 0, 0, 50);
     pop();
 
-    colorpoint = map(mouseX, 0, width, 0, 255);
-    stroke(colorpoint, colorpoint / 2, colorpoint);
+    // colorpoint = map(mouseX, 0, width, 0, 255);
+    // stroke(colorpoint, colorpoint / 2, colorpoint);
+    stroke(255);
     point(0, 0);
 
     // stroke(169, 169, 169);
